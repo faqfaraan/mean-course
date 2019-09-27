f4df2aab0c6714794cb0e61a6faf217b9327de77
Faraan <faqfaraan@gmail.com>
2019-09-27 19:25:46 -0400

    add post

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -27,8 +27,12 @@ export class PostsService {
 
   addPost(title: string, content: string) {
     const post: Post = {id: null, title, content};
-    this.posts.push(post);
-    this.postsUpdated.next([...this.posts]);
+    this.httpClient.post<{message: string}>('http://localhost:3000/api/posts', post)
+      .subscribe((responseData) => {
+        console.log(responseData);
+        this.posts.push(post);
+        this.postsUpdated.next([...this.posts]);
+      });
   }
 }
 
