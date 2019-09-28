f3db9be19a3d7d1b6ab2cc108b631471a7326c0d
Faraan Qureshi <faqfaraan@gmail.com>
2019-09-27 20:43:07 -0400

    initial mongo setup

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+
+const Post = require('./models/post');
 
 const app = express();
+
+mongoose.connect("mongodb+srv://faqfaraan:LmDK4D0fOQGEFHOd@cluster0-kkfcu.mongodb.net/node-angular?retryWrites=true&w=majority")
+  .then(() => {
+    console.log('Connected to database');
+  })
+  .catch(() => {
+    console.log('Connection failed');
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -14,8 +25,11 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
-  console.log(post);
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  post.save();
   res.status(201).json({
     message: 'Post added successfully'
   });
